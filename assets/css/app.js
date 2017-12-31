c6db51fd6fe7b27342b05bac59126f14619482e7
Wolfknightx <Wolfknightx@Gmail.com>
2017-12-30 22:22:59 -0600

    Update of material

--- assets/css/app.js
+++ assets/css/app.js
@@ -1,1 +1,22 @@
-var openHeroArr = ["Batman", "Nick Fury", "Spider-Man", "Wonder Woman", "Donatello"]
+var openHeroArr = ["Batman", "Nick Fury", "Spider-Man", "Wonder Woman", "Donatello"];
+var apiKey = lUF9860zUXEgSmf3JfJZ5EIGOin9lvdX;
+
+/* 
+use to check the queries
+https://developers.giphy.com/explorer/ 
+
+*/
+
+// In this case, the "this" keyword refers to the button that was clicked
+var searchEntry = $(this).attr("iEntryInput");
+
+// Constructing a URL to search Giphy for the name of the person who said the quote
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchEntry + "&api_key=" + apiKey + "&limit=10";
+
+// Render a button into the current display list.
+function createButton(){
+	
+}
+
+// When the user selects one of the buttons in the list display the gif tied to it.
+
